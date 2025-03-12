4329b1b4ef2e0d3831dabaabc105ed1aece2ae55
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-12 16:31:20 +0900

    Feat. DispatcherServlet으로 전환, annotation mapping 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,13 @@
 package tobyspring.helloboot;
 
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +15,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,27 +1,14 @@
 package tobyspring.helloboot;
-
-
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
@@ -30,36 +17,9 @@ public class HellobootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
 		// 웹 서버 생성 및 서블릿 컨텍스트 초기화 설정
-		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-
-				// 새로운 서블릿을 등록
-				servletContext.addServlet("frontController", new HttpServlet() {
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						// 인증, 보안, 다국어, 공통 기능
-						if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-							String name = req.getParameter("name");
-
-							HelloController helloController = applicationContext.getBean(HelloController.class);
-							String ret = helloController.hello(name);
-
-							// 응답 상태 코드 설정 (200 OK) 200 은 명시적으로 넣지 않아도 된다. servlet container가 error 가 안나면 200을 넣음.
-							// resp.setStatus(HttpStatus.OK.value());
-							// 응답 헤더 설정 (ContentType을 "text/plain"으로 설정)
-							// resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-							resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-							// 응답 본문에 "Hello Servlet" 출력
-							resp.getWriter().println(ret);
-						}
-						else {
-							resp.setStatus(HttpStatus.NOT_FOUND.value());
-						}
-					}
-				}).addMapping("/*"); // "/hello" 경로로 요청이 오면 해당 서블릿 실행
-			}
-		});
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext)).addMapping("/*");
+        });
 
 		// 웹 서버 시작
 		webServer.start();
