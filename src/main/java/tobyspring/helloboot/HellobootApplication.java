d34d01ff3bcc2f768e1f533d66bc484f33149f45
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-16 20:31:32 +0900

    Feat. 스프링 컨테이너안에 Web server 내장 시키기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,21 +8,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		// 스프링 컨테이너를 초기화 하는중에 부가적인 작업을 해야하면
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				// 톰캣 기반의 서블릿 웹 서버 팩토리 생성
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+				// 웹 서버 생성 및 서블릿 컨텍스트 초기화 설정
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)).addMapping("/*");
+				});
+
+				// 웹 서버 시작
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
-		// 톰캣 기반의 서블릿 웹 서버 팩토리 생성
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-		// 웹 서버 생성 및 서블릿 컨텍스트 초기화 설정
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext)).addMapping("/*");
-        });
-
-		// 웹 서버 시작
-		webServer.start();
 	}
 
 }
