98e896d7db7ce1f4caa29879e590e2b2e8ec9732
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-16 22:05:14 +0900

    Feat. 구성정보를 beanFactory로 구성해보기

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,8 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +14,23 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
+    private ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext =applicationContext;
+
+        System.out.println("sprong contaienr 자기 자신인 applicationContext가 자동으로 들어오나 확인" + applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        System.out.println("applicationContext" + applicationContext);
+//        this.applicationContext = applicationContext;
+//    }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -11,6 +11,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너를 초기화 하는중에 부가적인 작업을 해야하면
@@ -20,11 +29,13 @@ public class HellobootApplication {
 				super.onRefresh();
 
 				// 톰캣 기반의 서블릿 웹 서버 팩토리 생성
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//				dispatcherServlet.setApplicationContext(this);
 
 				// 웹 서버 생성 및 서블릿 컨텍스트 초기화 설정
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
 				});
 
 				// 웹 서버 시작
